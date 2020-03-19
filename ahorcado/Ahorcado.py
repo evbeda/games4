31e97625d89fe9b95e76ff138d060d75983f89ce
Agustin Gutierrez <agutierrez@eventbrite.com>
2020-03-19 12:19:33 -0300

    Tests for rights and worng letters

--- ahorcado/Ahorcado.py
+++ ahorcado/Ahorcado.py
@@ -6,9 +6,9 @@ class Ahorcado:
 		self.word = self.get_word_from_api()
 		self.lifes = 6
 		self.status_message = "Welcome to the game! Please choose one letter"
-		self.hidden_letters_message = ""
+		self.hidden_letters_message = "_ _ _ _ _ _ _ "
 		self.used_letters_message = ""
-		self.lifes_message = f"Lifes: {self.lifes}"
+		self.lifes_message = "Lifes: {}".format(self.lifes)
 
 	def get_word_from_api(self):
 		return "palabra"
@@ -18,8 +18,8 @@ class Ahorcado:
 			self.lifes = self.lifes - 1
 			self.status_message = "Wrong letter, you lose one life"
 		elif letter in self.word:
-			self.status_message = "Right letter! Choose another"
-			self.is_win = True
+			self.status_message = "Correct letter! Choose another"
+            self.is_win = True
 
 	def show_board(self):
-		return 
+		return self.status_message + '\n' + self.hidden_letters_message + '\n' + self.used_letters_message + '\n' + self.lifes_message
--- ahorcado/test_ahorcado.py
+++ ahorcado/test_ahorcado.py
@@ -1,7 +1,6 @@
 import unittest
-import string
-import random
 from Ahorcado import Ahorcado
+
 
 class TestAhorcado(unittest.TestCase):
 
@@ -10,28 +9,32 @@ class TestAhorcado(unittest.TestCase):
         self.game.word = "PALABRA"
 
     def test_force_win(self):
-        setted_word = set(self.game.word)
-        for letter in setted_word:
+        for letter in "PALABRA":
             self.game.play(letter)
         self.assertTrue(self.game.is_win)
 
+    def test_next_turn_win(self):
+        for letter in "PALABRA":
+            self.game.play(letter)
+        my_str = self.game.next_turn()
+        self.assertEqual(my_str, "You can't play!")
+
     def test_wrong_letter(self):
-        word = self.game.word
-        my_letter = random.choice(string.ascii_letters)
-        while my_letter in word:
-            my_letter = random.choice(string.ascii_letters)
-        self.game.play(my_letter)
+        self.game.play("Z")
         self.assertEqual(self.game.lifes, 5)
-        self.assertEqual(self.game.status_message, "Wrong letter, you lose one life")
-    
-    def test_simple_letter(self):
-        pass
+        my_board = "Wrong letter, you lose one life\n_ _ _ _ _ _ _\nZ \nLifes: 5"
+        self.assertEqual(self.game.show_board(), my_board)
+
+    def test_right_letter_p(self):
+        self.game.play('P')
+        self.assertEqual(self.game.lifes, 6)
+        my_board = "Correct letter! Choose another\nP _ _ _ _ _ _ \nP \nLifes: 6"
+        self.assertEqual(self.game.show_board(), my_board)
 
     def test_show_board(self):
-        self.assertEqual(self.game.status_message, "Welcome to the game! Please choose one letter")
-        self.assertEqual(self.game.hidden_letters_message, "_ _ _ _ _ _ _ ")
-        self.assertEqual(self.game.used_letters_message, "")
-        self.assertEqual(self.game.lifes_message, "Lifes: 6")
+        my_board = "Welcome to the game! Please choose one letter\n_ _ _ _ _ _ _ \n\nLifes: 6"
+        self.assertEqual(self.game.show_board(), my_board)
+
 
 if __name__ == "__main__":
 
