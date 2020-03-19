9a815652db2f5020ccc9427f5c49c7f5611ea76c
Axel Fernandez <axelfernandez@eventbrite.com>
2020-03-19 12:49:38 -0300

    Love Letter init Deck

--- test_suite.py
+++ test_suite.py
@@ -1,11 +1,13 @@
 import unittest
 from guess_number_game.test_guess_number_game import TestGuessNumberGame
+from love_letter.test_love_letter import TestLoveLetter
 
 
 def suite():
     test_suite = unittest.TestSuite()
     # GUESS NUMBER GAME
     test_suite.addTest(unittest.makeSuite(TestGuessNumberGame))
+    test_suite.addTest(unittest.makeSuite(TestLoveLetter))
     return test_suite
 
 
