913ad21ca5137a65142cc8d55f5e66a25e3d55b8
Agustin Gutierrez <agutierrez@eventbrite.com>
2020-03-19 09:59:38 -0300

    Test wrong letter w/ code

--- ahorcado/Ahorcado.py
+++ ahorcado/Ahorcado.py
@@ -3,11 +3,18 @@ class Ahorcado:
 	is_win = False
 
 	def __init__(self):
-		self.word = self.get_word()
+		self.word = self.get_word_from_api()
+		self.lifes = 6
+		self.status_message = ""
 
-	def get_word(self):
+	def get_word_from_api(self):
 		return "palabra"
 
-	def play(self, word):
-		self.is_win = True
+	def play(self, letter):
+		if letter not in self.word:
+			self.lifes = self.lifes - 1
+			self.status_message = "Wrong letter, you lose one life"
+		elif letter in self.word:
+			self.status_message = "Right letter! Choose another"
+			self.is_win = True
 
--- ahorcado/test_ahorcado.py
+++ ahorcado/test_ahorcado.py
@@ -1,4 +1,6 @@
 import unittest
+import string
+import random
 from Ahorcado import Ahorcado
 
 class TestAhorcado(unittest.TestCase):
@@ -8,11 +10,19 @@ class TestAhorcado(unittest.TestCase):
         #self.game.getword()
 
     def test_force_win(self):
-        word = self.game.get_word()
-        setted_word = set(word)
+        setted_word = set(self.game.word)
         for letter in setted_word:
             self.game.play(letter)
         self.assertTrue(self.game.is_win)
+
+    def test_wrong_letter(self):
+        word = self.game.word
+        my_letter = random.choice(string.ascii_letters)
+        while my_letter in word:
+            my_letter = random.choice(string.ascii_letters)
+        self.game.play(my_letter)
+        self.assertEqual(self.game.lifes, 5)
+        self.assertEqual(self.game.status_message, "Wrong letter, you lose one life")
 
 
 if __name__ == "__main__":
