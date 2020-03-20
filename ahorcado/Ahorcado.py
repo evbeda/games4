3aed9312223da25961e244d2905086364923f695
Agustin Gutierrez <agutierrez@eventbrite.com>
2020-03-20 11:16:07 -0300

    adding test for board and checks

--- ahorcado/Ahorcado.py
+++ ahorcado/Ahorcado.py
@@ -3,31 +3,15 @@ class Ahorcado:
 	def __init__(self):
 		self.word = self.get_word_from_api()
 		self.lifes = 6
-		self.is_win = False
-		self.is_lose = False
 		self.used_letters = []
 
-	def get_lifes(self):
-		return "Lifes: {}".format(self.lifes)
-
-	def get_word_from_api(self):
-		return "palabra"
-
-	def hidden_letters_message(self):
-		new_hidden_letters = []
-		for character in self.word:
-			if character in self.used_letters:
-				new_hidden_letters.append(character)
-			elif character not in self.used_letters:
-				new_hidden_letters.append("_")
-		return " ".join(new_hidden_letters)
-
 	def next_turn(self):
-		if self.is_win:
+		if not self.check_is_playing() and self.lifes > 0:
 			return "The player already won"
-		elif self.is_lose:
+		elif not self.check_is_playing() and self.lifes == 0:
 			return "The player already lost"
-		return "Please input a letter from A-Z"
+		else:
+			return "Please input a letter from A-Z"
 
 	def play(self, letter):
 		if self.check_input_used_letters(letter):
@@ -42,6 +26,18 @@ class Ahorcado:
 			self.set_used_letters(letter)
 			return "Correct letter! Choose another"
 
+	def get_lifes(self):
+		return "Lifes: {}".format(self.lifes)
+
+	def get_word_from_api(self):
+		return "palabra"
+
+	def set_used_letters(self, letter):
+		if letter not in self.used_letters:
+			self.used_letters.append(letter)
+		else:
+			pass
+
 	def check_input_used_letters(self, letter):
 		if letter in self.used_letters:
 			return True
@@ -54,12 +50,22 @@ class Ahorcado:
 		else:
 			return False
 
-	def set_used_letters(self, letter):
-		if letter not in self.used_letters:
-			self.used_letters.append(letter)
-		else:
-			pass
+	def check_is_playing(self):
+		if self.lifes > 0 and "_" not in self.hidden_letters_message():
+			return False
+		if self.lifes > 0:
+			return True
+		elif self.lifes == 0:
+			return False
 
+	def hidden_letters_message(self):
+		new_hidden_letters = []
+		for character in self.word:
+			if character in self.used_letters:
+				new_hidden_letters.append(character)
+			elif character not in self.used_letters:
+				new_hidden_letters.append("_")
+		return " ".join(new_hidden_letters)
 
 	@property
 	def board(self):
--- ahorcado/test_ahorcado.py
+++ ahorcado/test_ahorcado.py
@@ -1,5 +1,4 @@
 import unittest
-import unittest.mock
 from Ahorcado import Ahorcado
 
 
@@ -8,29 +7,29 @@ class TestAhorcado(unittest.TestCase):
     def setUp(self):
         self.game = Ahorcado()
         self.game.word = "PALABRA"
-    
+
     def test_next_turn_playing(self):
         self.assertEqual(self.game.next_turn(), "Please input a letter from A-Z")
-    
+
     def test_next_turn_win(self):
-        self.game.is_win = True
+        self.game.used_letters = ["P", "A", "L", "B", "R"]
         self.assertEqual(self.game.next_turn(), "The player already won")
-    
+
     def test_next_turn_lose(self):
-        self.game.is_lose = True
+        self.game.lifes = 0
         self.assertEqual(self.game.next_turn(), "The player already lost")
-    
+
     def test_play_correct(self):
         self.assertEqual(self.game.play("P"), "Correct letter! Choose another")
-    
+
     def test_play_repeat(self):
         self.game.play("P")
         self.assertEqual(self.game.play("P"), "Already tried that Letter! Try again")
-    
+
     def test_play_wrong(self):
         self.assertEqual(self.game.play("Z"), "Wrong letter, you lose one life")
         self.assertEqual(self.game.lifes, 5)
-    
+
     def test_get_lifes_changes(self):
         self.assertEqual(self.game.get_lifes(), "Lifes: 6")
         self.game.play("Z")
@@ -40,36 +39,36 @@ class TestAhorcado(unittest.TestCase):
         self.assertEqual(self.game.get_lifes(), "Lifes: 6")
         self.game.play("P")
         self.assertEqual(self.game.get_lifes(), "Lifes: 6")
-    
+
     def test_get_lifes_no_change_on_repeat(self):
         self.assertEqual(self.game.get_lifes(), "Lifes: 6")
         self.game.play("P")
         self.game.play("P")
         self.assertEqual(self.game.get_lifes(), "Lifes: 6")
-    
+
     def test_hidden_letters_message(self):
         self.assertEqual(self.game.hidden_letters_message(), "_ _ _ _ _ _ _")
-    
+
     def test_hidden_letters_message_P(self):
         self.game.play("P")
         self.assertEqual(self.game.hidden_letters_message(), "P _ _ _ _ _ _")
-    
+
     def test_hidden_letters_message_P_A(self):
         self.game.play("P")
         self.game.play("A")
         self.assertEqual(self.game.hidden_letters_message(), "P A _ A _ _ A")
-    
+
     def test_hidden_letters_message_P_A_A(self):
         self.game.play("P")
         self.game.play("A")
         self.game.play("A")
         self.assertEqual(self.game.hidden_letters_message(), "P A _ A _ _ A")
-    
+
     def test_hidden_letters_message_P_Z(self):
         self.game.play("P")
         self.game.play("Z")
         self.assertEqual(self.game.hidden_letters_message(), "P _ _ _ _ _ _")
-    
+
     def test_hidden_letters_message_PALABRA(self):
         self.game.play("P")
         self.game.play("A")
@@ -77,7 +76,7 @@ class TestAhorcado(unittest.TestCase):
         self.game.play("B")
         self.game.play("R")
         self.assertEqual(self.game.hidden_letters_message(), "P A L A B R A")
-    
+
     def test_hidden_letters_message_all_wrong(self):
         self.game.play("Z")
         self.game.play("Q")
@@ -86,6 +85,46 @@ class TestAhorcado(unittest.TestCase):
         self.game.play("V")
         self.assertEqual(self.game.hidden_letters_message(), "_ _ _ _ _ _ _")
 
+    def test_check_input_used_letters_true(self):
+        self.game.play("P")
+        self.assertTrue(self.game.check_input_used_letters("P"))
+
+    def test_check_input_used_letters_false(self):
+        self.game.play("Z")
+        self.assertFalse(self.game.check_input_used_letters("P"))
+
+    def test_check_input_word_true(self):
+        self.assertTrue(self.game.check_input_word("P"))
+
+    def test_check_input_word_false(self):
+        self.assertFalse(self.game.check_input_word("Z"))
+
+    def test_set_used_letters_P(self):
+        self.game.set_used_letters("P")
+        self.assertTrue("P" in self.game.used_letters)
+
+    def test_set_used_letters_Z(self):
+        self.game.set_used_letters("Z")
+        self.assertTrue("Z" in self.game.used_letters)
+
+    def test_board_min(self):
+        self.assertEqual(self.game.board, "_ _ _ _ _ _ _\n\nLifes: 6")
+
+    def test_board_w_one_letter(self):
+        self.game.play("P")
+        self.assertEqual(self.game.board, "P _ _ _ _ _ _\nP\nLifes: 6")
+
+    def test_board_w_two_letters(self):
+        self.game.play("P")
+        self.game.play("A")
+        self.assertEqual(self.game.board, "P A _ A _ _ A\nP A\nLifes: 6")
+
+    def test_board_w_three_letters_one_wrong(self):
+        self.game.play("P")
+        self.game.play("A")
+        self.game.play("Z")
+        self.game.play("W")
+        self.assertEqual(self.game.board, "P A _ A _ _ A\nP A Z W\nLifes: 4")
 
 
 if __name__ == "__main__":
