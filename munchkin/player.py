457d0301ff09c1a6af59bc8f9a5ba5afec95a4d0
aramis-eb <62262333+aramis-eb@users.noreply.github.com>
2020-03-20 13:05:44 -0300

    Create player for munchkin (#21)

---
+++ munchkin/player.py
@@ -0,0 +1,18 @@
+from human import Human
+
+class Player:
+	def __init__(self, name):
+		self._name = name
+		self._level = 1
+		self._race = Human()
+		self._class = None
+		self._objects = None
+	
+    def set_race (race):
+		self._race= race
+	
+    def win_help(self):
+        pass
+
+    @property
+	def level(self):
