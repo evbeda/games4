a9e2c69f3e260f81bb453cc01b38dc0258f45c10
Matias Sotelo <msotelo@eventbrite.com>
2020-03-20 10:05:31 -0300

    Add init player

--- test_suite.py
+++ test_suite.py
@@ -1,7 +1,8 @@
 import unittest
 from guess_number_game.test_guess_number_game import TestGuessNumberGame
 from senku.test_senku import TestSenku
-from love_letter.test_love_letter import TestDeck
+from love_letter.test_love_letter import TestDeck, TestPlayer
+
 
 def suite():
     test_suite = unittest.TestSuite()
@@ -9,6 +10,7 @@ def suite():
     test_suite.addTest(unittest.makeSuite(TestGuessNumberGame))
     test_suite.addTest(unittest.makeSuite(TestSenku))
     test_suite.addTest(unittest.makeSuite(TestDeck))
+    test_suite.addTest(unittest.makeSuite(TestPlayer))
     return test_suite
 
 
