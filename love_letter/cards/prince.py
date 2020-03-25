ca1a96f58f0dfe7d345d89a197ed2a0ebf04dfba
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-03-25 14:53:49 -0300

    LOVE LETTER: add __init__ method to every class

--- love_letter/cards/prince.py
+++ love_letter/cards/prince.py
@@ -2,7 +2,7 @@ from love_letter.card import Card
 
 
 class Prince(Card):
-
-    name = "Prince"
-    score = 5
-    description = "Player can choose any player (including themselves) to discard their hand and draw a new one. If the discarded card is the Princess, the discarding player is eliminated."
+    def __init__(self):
+        self.name = "Prince"
+        self.score = 5
+        self.description = "Player can choose any player (including themselves) to discard their hand and draw a new one. If the discarded card is the Princess, the discarding player is eliminated."
