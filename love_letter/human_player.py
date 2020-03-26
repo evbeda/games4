28153bcec0746bda82d7c3bc4791fd545415eefa
Federico Acosta <facosta@eventbrite.com>
2020-03-26 15:46:16 -0300

    loveletter reworked draw mechanics

--- love_letter/human_player.py
+++ love_letter/human_player.py
@@ -3,6 +3,6 @@ from love_letter.player import Player
 
 class HumanPlayer(Player):
 
-    def __init__(self, name):
+    def __init__(self, name, deck = None):
         super(HumanPlayer, self).__init__()
         self.name = name
--- love_letter/pc_player.py
+++ love_letter/pc_player.py
@@ -3,6 +3,6 @@ from love_letter.player import Player
 
 class PcPlayer(Player):
 
-    def __init__(self):
+    def __init__(self, deck = None):
         super(PcPlayer, self).__init__()
         self.name = "PC Player"
