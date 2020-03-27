2f7879853a8923c5142d8808579aa77ab657deac
Matias Sotelo <msotelo@eventbrite.com>
2020-03-27 13:02:02 -0300

    Adding action for Priest

--- love_letter/cards/priest.py
+++ love_letter/cards/priest.py
@@ -2,8 +2,13 @@ from love_letter.card import Card
 
 
 class Priest(Card):
+
     def __init__(self):
         super().__init__()
         self.name = "Priest"
         self.score = 2
         self.description = "Player is allowed to see another player's hand."
+
+    def execute_action(self, player, target=None):
+        card_to_show = target.show_card()
+        return card_to_show.__str__()
