55c55fb7067ce541e9be2f632bae57e342c31378
Axel Fernandez <axelfernandez@eventbrite.com>
2020-03-30 16:26:39 -0300

    Init Player start

--- ur_game/player.py
+++ ur_game/player.py
@@ -1,6 +1,9 @@
+from ur_game.cell import Cell
 from ur_game.token import Token
 
 
 class Player:
     def __init__(self):
         self.initial = [Token() for _ in range(7)]
+        self.start = [Cell() for _ in range(4)]
+
