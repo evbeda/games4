a5d199da8a50a254706acd9df44fcded2b5b2987
Gabriel Flores <gabrielf@eventbrite.com>
2020-03-30 11:29:27 -0300

    Hanoi Towers: Add Tower

--- hanoi_towers/test_hanoi_towers.py
+++ hanoi_towers/test_hanoi_towers.py
@@ -1,6 +1,7 @@
 import unittest
 
 from hanoi_towers.token import Token
+from hanoi_towers.tower import Tower
 
 
 class TestHanoiTower(unittest.TestCase):
@@ -8,3 +9,7 @@ class TestHanoiTower(unittest.TestCase):
     def test_token_basic_info(self):
         token = Token(5)
         self.assertEqual(token.size, 5)
+
+    def test_tower_basic_info(self):
+        tower = Tower()
+        self.assertEqual(len(tower.tokens), 0)
