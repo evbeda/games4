13bf47dd0aee0cf2b59837e0c362555e25fa6454
Agustin Gutierrez <agutierrez@eventbrite.com>
2020-03-30 15:47:18 -0300

    Hanoi Towers: adding the play method in HanoiTowers class

--- hanoi_towers/hanoi_towers.py
+++ hanoi_towers/hanoi_towers.py
@@ -1,9 +1,12 @@
 from .tower import Tower
+from .tower import InvalidMovement
+from .tower import EmptyTower
 
 
 class HanoiTowers:
 
     def __init__(self, cant_tokens):
+
         self.cant_tokens = cant_tokens
         self.tower1 = Tower(cant_tokens)
         self.tower2 = Tower()
@@ -15,3 +18,14 @@ class HanoiTowers:
             return "You won"
         else:
             return "Plase make your move"
+
+    def play(self, source_tower, target_tower):
+
+        try:
+            my_token = source_tower.remove_token()
+            target_tower.insert_token(my_token)
+        except InvalidMovement:
+            source_tower.insert_token(my_token)
+            return "Invalid move"
+        except EmptyTower:
+            return "Empty tower"
