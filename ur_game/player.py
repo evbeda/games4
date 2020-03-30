ed6c9b488dd6426c9b89b582d87dcdd3367ca85a
Matias Sotelo <msotelo@eventbrite.com>
2020-03-30 16:37:28 -0300

    UR: Add shared cells to player

--- ur_game/player.py
+++ ur_game/player.py
@@ -3,7 +3,8 @@ from ur_game.token import Token
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, shared=None):
+        self.shared = shared
         self.initial = [Token() for _ in range(7)]
         self.start = [Cell() for _ in range(4)]
 
