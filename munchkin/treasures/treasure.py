094308d53338d0e5b54ea514c86a23716015932b
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-03-30 15:34:38 -0300

    Munchkin: refactor inheritance for treasure cards

--- munchkin/treasures/treasure.py
+++ munchkin/treasures/treasure.py
@@ -1,8 +1,8 @@
 
 class Treasure:
 
-     def __init__(self):
-         self.type = "Treasure"
-         self.type_treasure = None
-         self.name= None
-
+     def __init__(self, name, bonus, value, used_by = None):
+         self.name = name
+         self.bonus = bonus
+         self.value = value
+         self.used_by = used_by
