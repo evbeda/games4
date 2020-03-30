f00bfbe9123f1f95882a81261fe84783d4bdade8
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-03-30 15:34:38 -0300

    Munchkin: refactor inheritance for treasure cards

---
+++ munchkin/treasures/weapon.py
@@ -0,0 +1,9 @@
+from munchkin.treasures.treasure import Treasure
+
+
+class Weapon(Treasure):
+
+    def __init__(self, size, cant_hands, name, bonus, value, used_by = None):
+        super().__init__(name, bonus, value, used_by)
+        self.size = size
+        self.cant_hands = cant_hands
