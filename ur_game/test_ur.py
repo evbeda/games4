9bc65450cd28c1f50a7cce33ebdce7af55a0d264
Axel Fernandez <axelfernandez@eventbrite.com>
2020-03-30 15:56:29 -0300

    Init Player

--- ur_game/test_ur.py
+++ ur_game/test_ur.py
@@ -1,5 +1,6 @@
 import unittest
 
+from ur_game.player import Player
 from ur_game.token import Token
 
 
@@ -8,3 +9,7 @@ class TestUr(unittest.TestCase):
     def test_token(self):
         token = Token()
         self.assertEqual(token.player, None)
+
+    def test_player(self):
+        player = Player()
+        self.assertEqual(len(player.initial), 7)
