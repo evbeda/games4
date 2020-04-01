f4d87867798b7e48aa460fb734761c329aa1a24f
German Hidalgo <ghidalgo-eb@eventbrite.com>
2020-04-01 14:47:48 -0300

    Added is playing property and roll dice method and tests

--- ur_game/ur.py
+++ ur_game/ur.py
@@ -1,5 +1,6 @@
 from ur_game.cell import Cell
 from ur_game.player import Player
+from random import randint
 
 
 class UrGame:
@@ -8,4 +9,13 @@ class UrGame:
         shared = [Cell() for _ in range(8)]
         self.players = [Player(shared) for _ in range(2)]
 
+    @property
+    def is_playing(self):
+        for player in self.players:
+            if len(player.final_stack) == 7:
+                return False
+        return True
 
+    @staticmethod
+    def roll_dices():
+        return randint(0, 4)
