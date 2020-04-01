26870d28bab56bc880543149206e3781f2284d97
Federico Acosta <facosta@eventbrite.com>
2020-04-01 14:21:30 -0300

    munchkin add various

--- munchkin/treasures/footwear.py
+++ munchkin/treasures/footwear.py
@@ -2,5 +2,5 @@ from munchkin.treasures.treasure import Treasure
 
 class Footwear(Treasure):
 
-    def __init__(self, name, bonus, value, used_by = None, is_big = False):
+    def __init__(self, name, bonus, value, used_by = None, is_big = False, flee_bonus = 0):
         super().__init__(name, bonus, value, used_by)
