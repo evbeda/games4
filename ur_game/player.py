4793ee8182f34c2c636305b6af7c019291916199
Agustin Gutierrez <agutierrez@eventbrite.com>
2020-04-01 10:25:53 -0300

    Adding the index attribute to token class

--- ur_game/player.py
+++ ur_game/player.py
@@ -5,8 +5,7 @@ from ur_game.token import Token
 class Player:
     def __init__(self, shared=None):
         self.shared = shared
-        self.initial = [Token() for _ in range(7)]
+        self.initial = [Token(i) for i in range(7)]
         self.final_stack = []
         self.start = [Cell() for _ in range(4)]
         self.finish = [Cell() for _ in range(2)]
-
