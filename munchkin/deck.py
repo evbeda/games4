ad0d7aecce84873e7005d82234569aff4c1ad4b3
Agustin Ramis <aramis@eventbrite.com>
2020-04-02 15:04:34 -0300

    Munchkin: Add test for deck

--- munchkin/deck.py
+++ munchkin/deck.py
@@ -16,18 +16,14 @@ class Deck:
 
     def __init__(self):
         self._cards = []
-        self._discard_cards = []
+        self.discard_cards = []
 
     @property
     def cards(self):
         return self._cards
 
-    @property
-    def discard_cards(self):
-        return self._discard_cards
-
     def add_discard(self, card_to_add):
-        self._discard_cards.append(card_to_add)
+        self.discard_cards.append(card_to_add)
 
     def add_cards(self, cards_to_add):
         self._cards.extend(cards_to_add)
@@ -36,7 +32,8 @@ class Deck:
         random.shuffle(self._cards)
 
     def reset_cards(self):
-        self._cards = self._discard_cards
+        self._cards = self.discard_cards
+        self.discard_cards = []
         self.shuffle_deck()
 
     def draw_card(self):
