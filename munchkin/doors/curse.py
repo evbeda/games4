336ebf3fd05f1ad5532709a18978a9c0fb26f1e1
fgomez-eb <fgomez@eventbrite.com>
2020-04-03 15:50:26 -0300

    munchkin: remove curse

--- munchkin/doors/curse.py
+++ munchkin/doors/curse.py
@@ -1,15 +1,5 @@
-from munchkin.doors.door import Door
-
-
-class Curse(Door):
-
-    def __init__(self, name, target, description_effect):
-        super().__init__(name)
-        self.target = exec(target+'()')
-        self.description_effect = description_effect
-
-    def apply_effect(self, player):
-        player.on_board
-
-    def __str__(self):
-        return super().__str__() + '\n Description Effect: ' + self.description_effect
+# from munchkin.doors.door import Door
+#
+#
+# class Curse(Door):
+#     pass
