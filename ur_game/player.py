693c82b8c56187c75fefdd45a6397874c8731e17
German Hidalgo <ghidalgo-eb@eventbrite.com>
2020-04-03 16:28:16 -0300

    ur game: board string method added, and also added str method to every class

--- ur_game/player.py
+++ ur_game/player.py
@@ -3,7 +3,7 @@ from ur_game.token import Token
 
 
 class Player:
-    def __init__(self, shared=[]):
+    def __init__(self, shared=[], id_number=0):
         self.shared = shared
         self.initial = [Token(i, self) for i in range(7)]
         self.final_stack = []
@@ -12,6 +12,12 @@ class Player:
         self.finish = [Cell() for _ in range(2)]
         self.finish[0].set_special()
         self.addition_turn = False
+        self.id_number = id_number + 1
+        self.type = None
+        if self.id_number == 1:
+            self.token_type = "O"
+        else:
+            self.token_type = "X"
 
     def validate_movement_from_cell(self, from_index):
         from_cell = self.get_cell_by_index(from_index)
@@ -72,6 +78,49 @@ class Player:
         else:
             from_cell = self.validate_movement_from_cell(index_token)
             self.move_token_from_cell_to_cell(from_cell, to_cell)
+
+    def initial_and_final_arr_str(self):
+        initial_arr = "["
+        ini = 4
+        for cell in reversed(self.start):
+            if not cell.is_empty and ini > 1:
+                initial_arr += " {},".format(cell.__str__())
+            elif not cell.is_empty and ini == 1:
+                initial_arr += " {}".format(cell.__str__())
+            elif cell.is_special and ini > 1:
+                initial_arr += "*{},".format(ini)
+            elif not cell.is_special and ini > 1:
+                initial_arr += " {},".format(ini)
+            else:
+                initial_arr += " {}".format(ini)
+            ini -= 1
+        initial_arr += "]"
+
+        final_arr = "["
+        ini = 14
+        for cell in reversed(self.finish):
+            if cell.is_empty and ini == 14:
+                final_arr += "*{},".format(ini)
+            elif cell.is_empty and ini == 13:
+                final_arr += " {}".format(ini)
+            else:
+                initial_arr += " {},".format(cell.__str__())
+            ini -= 1
+        final_arr += "]"
+
+        return initial_arr + " " * 7 + final_arr
+
+    def __str__(self):
+        return "Player{} number of initial  tokens: {}\n" \
+               "Player{} number of finished tokens: {}\n" \
+               "Player{} type of token: {}".format(
+                self.id_number,
+                len(self.initial),
+                self.id_number,
+                len(self.final_stack),
+                self.id_number,
+                self.token_type
+        )
 
 
 class InvalidMovementException(Exception):
